c4de56a0a7884ffd0105787f730b3bf9c6458edf
Sam Sendelbach <sbsends@gmail.com>
2018-06-19 11:54:12 -0500

    corrected unzipping clearmap_resources

--- clearmap_install.sh
+++ clearmap_install.sh
@@ -8,7 +8,7 @@ module load gcc/6.4.0
 mkdir clearmap
 cd clearmap
 wget https://idiscodotinfo.files.wordpress.com/2016/05/clearmap_ressources_mouse_brain.zip
-unzip -d clearmap_ressources_mouse_brain.zip
+unzip clearmap_ressources_mouse_brain.zip
 rm clearmap_ressources_mouse_brain.zip
 git clone https://github.com/ChristophKirst/ClearMap.git
 wget https://github.com/SuperElastix/elastix/releases/download/4.9.0/elastix-4.9.0-linux.tar.bz2
